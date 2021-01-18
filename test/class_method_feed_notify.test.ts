db9fd67e18bc9b20345add83eb182ae31e69b1b8
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-01-19 08:46:32 +0900

    refactor: Classに書き換えて、分割

--- test/class_method_feed_notify.test.ts
+++ test/class_method_feed_notify.test.ts
@@ -1,13 +1,25 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as ClassMethodFeedNotify from '../lib/class_method_feed_notify-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as ClassMethodFeedNotify from "../lib/classMethodFeedNotifyLPipelineStack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new ClassMethodFeedNotify.ClassMethodFeedNotifyStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new ClassMethodFeedNotify.ClassMethodFeedNotifyStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
