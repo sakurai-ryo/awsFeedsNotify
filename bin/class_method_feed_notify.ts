259b7981f45301805978eef7dad733721bcc76fc
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-01-19 08:27:02 +0900

    init

--- bin/class_method_feed_notify.ts
+++ bin/class_method_feed_notify.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ClassMethodFeedNotifyStack } from '../lib/class_method_feed_notify-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { classMethodFeedNotifyPipeline } from "../lib/class_method_feed_notify-stack";
 
 const app = new cdk.App();
-new ClassMethodFeedNotifyStack(app, 'ClassMethodFeedNotifyStack');
+
+async function buildApp(): Promise<void> {
+  const app = new cdk.App();
+  await classMethodFeedNotifyPipeline(app, "ClassMethodFeedNotifyStack");
+}
+
+buildApp();
